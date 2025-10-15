46c20584ff272a642c976cd702588686d4294fba
unknown <105841103622@student.unismuh.ac.id>
2025-10-15 19:24:21 +0800

    perbaikan

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  server: {
-    // Memberitahu Vite untuk mendengarkan di semua alamat IP yang tersedia
-    host: '0.0.0.0', 
-    
-    // Ini adalah kunci utama: secara eksplisit izinkan host dari ngrok
-    // Tanda titik di depan berfungsi sebagai wildcard, mengizinkan semua subdomain
-    allowedHosts: ['.ngrok-free.app'] 
-  },
 })
